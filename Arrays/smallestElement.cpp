4a8971da4504b6a04d290306b061d01cd77c7557
Animesh-Maiti <animeshmaiti862@gmail.com>
2026-06-22 22:29:30 +0530

    modified to show index also

--- Arrays/smallestElement.cpp
+++ Arrays/smallestElement.cpp
@@ -6,13 +6,16 @@ using namespace std;
 int main(){
 
     int arr[]={7,8,1,5};
-    int smallest = INT_MAX;
+    int smallest = INT_MAX,index;
 
     for(int i=0; i<4;i++){
-        smallest = min(smallest,arr[i]);
+        if (smallest>arr[i]){
+            smallest = arr[i];
+            index=i;
+        }
     }
 
-    cout<<"smalesst number ="<<smallest<<endl;
+    cout<<"smalesst number ="<<smallest<<" index =" <<index<<endl;
 
     return 0;
 }
